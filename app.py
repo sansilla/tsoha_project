bcd12ae06c2e790e030f2c2e40a83b08824bd9d2
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-11-08 15:18:09 +0200

    testing app start

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 #Vasta testailua!
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///sansilla"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-	return "Halojata! :)"
+	result = db.session.execute("SELECT name FROM bands")
+	bands = result.fetchall()
+	return render_template("index.html", bands=bands)
