01f1a3a503e50e419307105e7c86deb7c5be7373
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-11-08 16:30:57 +0200

    modified app.py and moved index

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///sansilla"
@@ -9,6 +10,7 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-	result = db.session.execute("SELECT name FROM bands")
+	result = db.session.execute(text("SELECT name FROM bands"))
 	bands = result.fetchall()
 	return render_template("index.html", bands=bands)
+	#return "Hallooo :("
