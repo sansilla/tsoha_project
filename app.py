3f740f792fa070ff19bc50dad70972ad7c2c08c4
sansilla <127957725+sansilla@users.noreply.github.com>
2023-11-09 10:54:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask import redirect, render_template, request #tulee siirtymään
+#from flask import redirect, render_template, request SIIRRETTY ROUTESIIN
 #from flask_sqlalchemy import SQLAlchemy SIIRRETTY DATABASEEN
-from sqlalchemy.sql import text #tulee siirtymään
+#from sqlalchemy.sql import text SIIRRETTY ROUTESIIN
 from os import getenv
 
 app = Flask(__name__)
@@ -9,9 +9,11 @@ app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///sansilla" EI VARMAAN TARVITA ENÄÄ?
 #db = SQLAlchemy(app) SIIRRETTY DATABASEEN
 
-@app.route("/")
-def index():
-	result = db.session.execute(text("SELECT name FROM bands"))
-	bands = result.fetchall()
-	return render_template("index.html", bands=bands)
+#@app.route("/") SIIRRETTY ROUTESIIN
+#def index():
+	#result = db.session.execute(text("SELECT name FROM bands"))
+	#bands = result.fetchall()
+	#return render_template("index.html", bands=bands)
 	#return "Hallooo :("
+
+import routes
