919888b09d42581b55e445880af0d076111ac0e4
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-11-10 09:23:30 +0200

    edited bands and routes

--- bands.py
+++ bands.py
@@ -4,3 +4,7 @@ from sqlalchemy.sql import text
 def show_all_bands():
 	sql = "SELECT name FROM bands ORDER BY name"
 	return db.session.execute(text(sql)).fetchall()
+
+def show_band_info(band_id):
+	sql = "SELECT bands.name, info.info_text FROM bands, info WHERE bands.id=info.band_id"
+	return db.session.execute(sql).fetchone()
--- routes.py
+++ routes.py
@@ -9,3 +9,8 @@ def index():
 	#result = db.session.execute(text("SELECT name FROM bands"))
 	#bands = result.fetchall()
 	return render_template("index.html", bands=bands.show_all_bands())
+
+@app.route("/band_info")
+def show_info(band_id):
+	info = bands.show_band_info(band_id)
+	return render tempalte(???) # LOPPUUN
