3c63b68618741599102e5e8a4dca89110472bd9a
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-11-13 11:42:06 +0200

    getting band infos to show right

--- bands.py
+++ bands.py
@@ -6,5 +6,5 @@ def show_all_bands():
 	return db.session.execute(text(sql)).fetchall()
 
 def show_band_info(band_name):
-	sql = "SELECT band_name, info_text FROM info WHERE band_name=band_name"
-	return db.session.execute(text(sql)).fetchone()
+	sql = "SELECT band_name, info_text FROM info WHERE band_name=:band_name"
+	return db.session.execute(text(sql), {"band_name": band_name}).fetchone()
