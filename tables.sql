a507113a0c9fcc30a5d12ba41d7e429971857b5c
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-12-11 17:15:55 +0200

    inserting stuff into tables for later testing

--- tables.sql
+++ tables.sql
@@ -7,3 +7,6 @@ CREATE TABLE reviews (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, b
 CREATE TABLE info (id SERIAL PRIMARY KEY, band_name TEXT REFERENCES bands(name), info_text TEXT);
 
 CREATE TABLE favourites (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, band_id INTEGER REFERENCES bands);
+
+INSERT INTO bands (name) VALUES ('Queen');
+INSERT INTO info (band_name, info_text) VALUES ('Queen', 'Rockin klassikkobändi! Perustettu Englannissa vuonna 1970. Kokoonpanoon kuului laulaja Freddie Mercury, kitaristi Brian May, rumpali Roger Taylor ja basisti John Deacon.');
